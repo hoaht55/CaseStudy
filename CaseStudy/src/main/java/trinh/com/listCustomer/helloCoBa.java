ba4e596ab374427259b56d1635d8295e628e422e
Hung CR <hungcr.uet@gmail.com>
2014-04-07 16:53:01 +0700

    Coba

--- CaseStudy/src/main/java/trinh/com/listCustomer/helloCoBa.java
+++ CaseStudy/src/main/java/trinh/com/listCustomer/helloCoBa.java
@@ -1,7 +1,7 @@
 package trinh.com.listCustomer;
 
-/**
- * Created by Master on 4/7/14.
- */
 public class helloCoBa {
+    public static void main(String[] args) {
+        System.out.println("HungCR2163");
+    }
 }
